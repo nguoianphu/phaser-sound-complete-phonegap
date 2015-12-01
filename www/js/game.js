2504979cc8aa19c19cb27dbaa9bc78fcbf357e62
nguoianphu <nguoianphu@gmail.com>
2015-12-01 21:54:56 +0700

    add .ogg file

--- www/js/game.js
+++ www/js/game.js
@@ -28,8 +28,7 @@ function preload() {
 
 
     game.load.audio('explosion', 'assets/audio/SoundEffects/explosion.mp3');
-    game.load.audio('sword', 'assets/audio/SoundEffects/sword.mp3');
-    game.load.audio('blaster', 'assets/audio/SoundEffects/blaster.mp3');
+    game.load.audio('fanfare', 'assets/audio/SoundEffects/fanfare3.ogg');
 
 /*
 var explosion = new Media('/android_asset/www/assets/audio/SoundEffects/explosion.mp3');
@@ -39,14 +38,12 @@ var blaster= new Media('/android_asset/www/assets/audio/SoundEffects/blaster.mp3
 }
 
 var explosion;
-var sword;
-var blaster;
+var fanfare;
 
 var text;
 
 var text1;
 var text2;
-var text3;
 
 function create() {
 
@@ -58,14 +55,13 @@ function create() {
 
 
     explosion = game.add.audio('explosion');
-    sword = game.add.audio('sword');
-    blaster = game.add.audio('blaster');
+    fanfare = game.add.audio('fanfare');
 
     //  Being mp3 files these take time to decode, so we can't play them instantly
     //  Using setDecodedCallback we can be notified when they're ALL ready for use.
     //  The audio files could decode in ANY order, we can never be sure which it'll be.
 
-    game.sound.setDecodedCallback([ explosion, sword, blaster ], start, this);
+    game.sound.setDecodedCallback([ explosion, fanfare ], start, this);
 
 }
 
@@ -73,28 +69,24 @@ var keys;
 
 function start() {
 
-    text.text = 'Press 1, 2 or 3';
+    text.text = 'Press on text\n for sound';
 
     var style = { font: "48px Arial", fill: "#cdba52", align: "center" };
 
-    text1 = game.add.text(game.world.centerX, 250, "Blaster: Stopped", style);
+
+    text1 = game.add.text(game.world.centerX, 250, "Explosion (.mp3): Stopped", style);
     text1.anchor.set(0.5);
 
-    text2 = game.add.text(game.world.centerX, 350, "Explosion: Stopped", style);
+    text2 = game.add.text(game.world.centerX, 450, "Fanfare (.ogg): Stopped", style);
     text2.anchor.set(0.5);
 
-    text3 = game.add.text(game.world.centerX, 450, "Sword: Stopped", style);
-    text3.anchor.set(0.5);
-
     explosion.onStop.add(soundStopped, this);
-    sword.onStop.add(soundStopped, this);
-    blaster.onStop.add(soundStopped, this);
+    fanfare.onStop.add(soundStopped, this);
 
-    keys = game.input.keyboard.addKeys({ blaster: Phaser.Keyboard.ONE, explosion: Phaser.Keyboard.TWO, sword: Phaser.Keyboard.THREE });
+    keys = game.input.keyboard.addKeys({ explosion: Phaser.Keyboard.ONE, fanfare: Phaser.Keyboard.TWO });
 
-    keys.blaster.onDown.add(playFx, this);
+    keys.fanfare.onDown.add(playFx, this);
     keys.explosion.onDown.add(playFx, this);
-    keys.sword.onDown.add(playFx, this);
 
     //  And for touch devices you can also press the top, middle or bottom of the screen
     game.input.onDown.add(onTouch, this);
@@ -103,22 +95,15 @@ function start() {
 
 function onTouch(pointer) {
 
-    var b = game.height / 3;
+    var b = game.height / 2;
 
-    if (pointer.y < b)
+    if (pointer.y > b)
     {
-        playFx(keys.blaster);
-    //    playAudio('assets/audio/SoundEffects/blaster.mp3');
-    }
-    else if (pointer.y > b * 2)
-    {
-        playFx(keys.sword);
-       // playAudio('assets/audio/SoundEffects/sword.mp3');
+        playFx(keys.fanfare);
     }
     else
     {
         playFx(keys.explosion);
-     //  playAudio('assets/audio/SoundEffects/explosion.mp3');
     }
 
 }
@@ -130,37 +115,30 @@ function playFx(key) {
     switch (key.keyCode)
     {
         case Phaser.Keyboard.ONE:
-            text1.text = "Blaster: Playing";
-            blaster.play();
-            break;
-
-        case Phaser.Keyboard.TWO:
-            text2.text = "Explosion: Playing";
+            text1.text = "Explosion (.mp3): Playing";
             explosion.play();
             break;
 
-        case Phaser.Keyboard.THREE:
-            text3.text = "Sword: Playing";
-            sword.play();
+        case Phaser.Keyboard.TWO:
+            text2.text = "Fanfare (.ogg): Playing";
+            fanfare.play();
             break;
+
     }
 
 }
 
 function soundStopped(sound) {
 
-    if (sound === blaster)
+	if (sound === explosion)
     {
-        text1.text = "Blaster: Complete";
+        text1.text = "Explosion (.mp3): Complete";
     }
-    else if (sound === explosion)
+	else if (sound === fanfare)
     {
-        text2.text = "Explosion: Complete";
+        text2.text = "Fanfare (.ogg): Complete";
     }
-    else if (sound === sword)
-    {
-        text3.text = "Sword: Complete";
-    }
+
 
 }
 
